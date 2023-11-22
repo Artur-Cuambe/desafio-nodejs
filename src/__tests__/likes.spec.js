5d64ff354224056a3216c569c6247a2af47d2ee2
Artur-Cuambe <arturcuamb@gmail.com>
2023-11-22 12:08:26 +0200

    frist commit

--- src/__tests__/likes.spec.js
+++ src/__tests__/likes.spec.js
@@ -27,7 +27,6 @@ beforeEach(async () => {
     }
 });
 describe('Testes de unidade - Controladores', () => {
- 
   it('should be able to give a like to the repository', async () => {
       const createdRepository = await Repository.create({
         url: "https://github.com/WhireLab/About",
@@ -43,38 +42,3 @@ describe('Testes de unidade - Controladores', () => {
       expect(response.body.likes).toBe(1);
   }, 10000);
 });
-
-// const request = require("supertest");
-// const app = require("../app");
-
-// describe("Likes", () => {
-//   it("should be able to give a like to the repository", async () => {
-//     const repository = await request(app)
-//       .post("/repositories")
-//       .send({
-//         url: "https://github.com/WhireLab/About",
-//         title: "About",
-//         techs: ["Node", "Express", "TypeScript"],
-//       });
-
-//     let response = await request(app).post(
-//       `/repositories/${repository.body.id}/like`
-//     );
-
-//     expect(response.body).toMatchObject({
-//       likes: 1,
-//     });
-
-//     response = await request(app).post(
-//       `/repositories/${repository.body.id}/like`
-//     );
-
-//     expect(response.body).toMatchObject({
-//       likes: 2,
-//     });
-//   });
-
-//   it("should not be able to like a repository that does not exist", async () => {
-//     await request(app).post(`/repositories/123/like`).expect(400);
-//   });
-// });
